5725d1e9d7b091e4ce2f0bceffefd6379792e267
“Dipali <dtank150@rku.ac.in>
2023-06-28 11:05:04 +0530

    first commit

---
+++ WebApiCore/WebApiCore/Migrations/20230612103515_Intial1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiCore.Migrations
+{
+    public partial class Intial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "TblEmployee",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "TblEmployee");
+        }
+    }
+}
